5f11fee6306f40e45e964d258819d3b6d702984b
linda89821 <77847533+linda89821@users.noreply.github.com>
2022-11-14 04:45:00 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -41,7 +41,7 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  event.respondWith(caches.open(cacheName).then((cache) => {
+  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
     // B8. TODO - If the request is in the cache, return with the cached version.
     //            Otherwise fetch the resource, add it to the cache, and return
     //            network response.
